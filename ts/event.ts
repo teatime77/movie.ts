e6b1e09912bd1f0c8eecbac841aeee0e5da8a503
teatime77 <teatime77@live.jp>
2024-10-21 21:33:47 +0900

    add movie_ui.ts

--- ts/event.ts
+++ ts/event.ts
@@ -1,14 +1,9 @@
 namespace movie_ts {
 //
-type Block = layout_ts.Block;
-type Button = layout_ts.Button;
 
 let theDoc : firebase_ts.DbDoc | undefined;
 export let root : layout_ts.Grid;
 
-const $flex = layout_ts.$flex;
-const $grid = layout_ts.$grid;
-const $block = layout_ts.$block;
 const $button = layout_ts.$button;
 
 export async function bodyOnLoad(){
@@ -48,17 +43,6 @@ export async function bodyOnLoad(){
 
     layout_ts.initLayout(root);
 
-    if(edit_mode){
-
-        $("movie-write-db").addEventListener("click", (ev : MouseEvent)=>{
-            writeDB();
-        });
-
-        $("movie-show-firebase").addEventListener("click", (ev : MouseEvent)=>{
-            $dlg("firebase-menu").showModal();        
-        });
-    }
-
     i18n_ts.initLanguageBar($("language-bar"));
 
     plane_ts.initPlane(plane, root);
@@ -76,127 +60,6 @@ export async function bodyOnLoad(){
     msg(`navigator:${navigator.appVersion}`);
 }
 
-function makeEditGrid(plane : plane_ts.Plane, play_button : Button, show_contents_button : Button) : layout_ts.Grid {
-    const root = $grid({
-        rows     : "25px 25px 100% 25px",
-        children:[
-            $block({
-                id : "language-bar",
-                children : [],
-                backgroundColor : "chocolate",
-            })
-            ,
-            $grid({
-                columns  : "50% 50%",
-                children: [
-                    plane.menu_block
-                    ,
-                    $flex({
-                        children : [
-                            play_button
-                            ,
-                            $button({
-                                id : "movie-write-db",
-                                text : "Write DB"
-                            })
-                            ,
-                            $button({
-                                id : "movie-show-firebase",
-                                text : "firebase"
-                            })
-                            ,
-                            show_contents_button
-                            ,
-                            $button({
-                                text : "log",
-                                click : async (ev:MouseEvent)=>{
-                                    layout_ts.Log.show(ev);
-                                }
-                            })
-                        ],
-                        backgroundColor : "violet",
-                    })
-                ]
-            })
-            ,
-            $grid({
-                columns  : "50px 50% 50% 300px",
-
-                children : [
-                    plane.tool_block
-                    ,
-                    plane.text_block
-                    ,
-                    plane.canvas_block
-                    ,
-                    plane.property_block
-                ]
-            })
-            ,
-            plane.shapes_block
-        ]
-    });
-
-    return root;    
-}
-
-function makePlayGrid(plane : plane_ts.Plane, play_button : Button, show_contents_button : Button) : layout_ts.Grid {
-    let content_grid : layout_ts.Grid;
-    if(window.innerHeight < window.innerWidth ){
-        content_grid = $grid({
-            columns  : "50% 50%",
-
-            children : [
-                plane.text_block
-                ,
-                plane.canvas_block
-            ]
-        })
-    }
-    else{
-
-        content_grid = $grid({
-            rows  : "50% 50%",
-
-            children : [
-                plane.canvas_block
-                ,
-                plane.text_block
-            ]
-        })
-    }
-    
-    const root = $grid({
-        rows     : "25px 25px 100% 25px",
-        children:[
-            $block({
-                id : "language-bar",
-                children : [],
-                backgroundColor : "chocolate",
-            })
-            ,
-            $flex({
-                children : [
-                    play_button
-                    ,
-                    show_contents_button
-                    ,
-                    $button({
-                        text : "log",
-                        click : async (ev:MouseEvent)=>{
-                            layout_ts.Log.show(ev);
-                        }
-                    })
-                ],
-                backgroundColor : "violet",
-            })
-            ,
-            content_grid
-        ]
-    });
-
-    return root;    
-}
 async function readDoc(id : number) {
     msg(`id:${id}`);
     theDoc = await firebase_ts.getDoc(id);
@@ -208,7 +71,7 @@ async function readDoc(id : number) {
     }
 }
 
-async function writeDB(){
+export async function writeDB(){
     let default_name = (theDoc == undefined ? "" : theDoc.name);
 
     let name = prompt("Enter the document name.", default_name);
@@ -245,4 +108,5 @@ async function writeDB(){
 export function SignUp(){
     $dlg("sign-up").showModal();
 }
+
 }
---
+++ ts/movie_ui.ts
@@ -0,0 +1,144 @@
+namespace movie_ts {
+//
+type Block = layout_ts.Block;
+type Button = layout_ts.Button;
+
+const $flex = layout_ts.$flex;
+const $grid = layout_ts.$grid;
+const $block = layout_ts.$block;
+const $button = layout_ts.$button;
+
+export function makeEditGrid(plane : plane_ts.Plane, play_button : Button, show_contents_button : Button) : layout_ts.Grid {
+    const root = $grid({
+        rows     : "25px 25px 100% 25px",
+        children:[
+            $block({
+                id : "language-bar",
+                children : [],
+                backgroundColor : "chocolate",
+            })
+            ,
+            $grid({
+                columns  : "50% 50%",
+                children: [
+                    plane.menu_block
+                    ,
+                    $flex({
+                        children : [
+                            play_button
+                            ,
+                            $button({
+                                text : "Write DB",
+                                click : async (ev:MouseEvent)=>{
+                                    writeDB();
+                                }
+                            })
+                            ,
+                            $button({
+                                text : "Back up",
+                                click : async (ev:MouseEvent)=>{
+                                    await firebase_ts.BackUp(1);
+                                }
+                            })
+                            ,
+                            $button({
+                                text : "firebase",
+                                click : async (ev:MouseEvent)=>{
+                                    $dlg("firebase-menu").showModal();        
+                                }
+                            })
+                            ,
+                            show_contents_button
+                            ,
+                            $button({
+                                text : "log",
+                                click : async (ev:MouseEvent)=>{
+                                    layout_ts.Log.show(ev);
+                                }
+                            })
+                        ],
+                        backgroundColor : "violet",
+                    })
+                ]
+            })
+            ,
+            $grid({
+                columns  : "50px 50% 50% 300px",
+
+                children : [
+                    plane.tool_block
+                    ,
+                    plane.text_block
+                    ,
+                    plane.canvas_block
+                    ,
+                    plane.property_block
+                ]
+            })
+            ,
+            plane.shapes_block
+        ]
+    });
+
+    return root;    
+}
+
+export function makePlayGrid(plane : plane_ts.Plane, play_button : Button, show_contents_button : Button) : layout_ts.Grid {
+    let content_grid : layout_ts.Grid;
+    if(window.innerHeight < window.innerWidth ){
+        content_grid = $grid({
+            columns  : "50% 50%",
+
+            children : [
+                plane.text_block
+                ,
+                plane.canvas_block
+            ]
+        })
+    }
+    else{
+
+        content_grid = $grid({
+            rows  : "50% 50%",
+
+            children : [
+                plane.canvas_block
+                ,
+                plane.text_block
+            ]
+        })
+    }
+    
+    const root = $grid({
+        rows     : "25px 25px 100% 25px",
+        children:[
+            $block({
+                id : "language-bar",
+                children : [],
+                backgroundColor : "chocolate",
+            })
+            ,
+            $flex({
+                children : [
+                    play_button
+                    ,
+                    show_contents_button
+                    ,
+                    $button({
+                        text : "log",
+                        click : async (ev:MouseEvent)=>{
+                            layout_ts.Log.show(ev);
+                        }
+                    })
+                ],
+                backgroundColor : "violet",
+            })
+            ,
+            content_grid
+        ]
+    });
+
+    return root;    
+}
+
+}
