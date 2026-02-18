d8ef975b4c4dfdc86c2e9394f19b56a3611ca8f3
teatime77 <teatime77@live.jp>
2026-02-18 10:05:35 +0900

    outDir => ./dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig, mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { baseConfig } from '../vite.config.base';
 import { resolve } from 'path';
 
 export default defineConfig(
   mergeConfig(baseConfig, {
+    plugins: [tsconfigPaths()],
     build: {
       minify: false,
       lib: {
         entry: resolve(__dirname, 'ts/movie_event.ts'),
-        fileName: 'index',
+        fileName: 'movie',
         formats: ['es']
       },
-      outDir: './movie.ts/public/lib/movie'
+      outDir: '../dist'
     }
   })
 );
